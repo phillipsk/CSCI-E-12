2e2c9e5e93ca29df586dd2211cd1133b247607d7
phillipsk <inquiries@kevinphillips.email>
2016-04-03 12:00:53 -0400

    Week 9

---
+++ Week 9/gallery/js/gallery.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("a[rel=fancybox_group]").fancybox({
+        'transitionIn'		: 'none',
+        'transitionOut'		: 'none',
+        'titlePosition' 	: 'over',
+        'titleFormat'		: function(title, currentArray, currentIndex, currentOpts) {
+            return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+        }
+    });
+
+});
