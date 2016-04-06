5822b6eb5bd79331fa471823b11df381b30f416a
phillipsk <inquiries@kevinphillips.email>
2016-04-05 23:20:39 -0400

    finished contact us assignment and validated

---
+++ Week 9/contact_us_form/js/contact.js
@@ -0,0 +1,6 @@
+// Simple one line JS code to action the Form Validation plugin
+// http://jqueryvalidation.org/documentation/
+
+$(document).ready(function () {
+    $("#contactForm").validate();
+});
