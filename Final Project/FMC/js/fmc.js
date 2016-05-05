143cd30833ecedc2031a457e374195001f45bea4
phillipsk <inquiries@kevinphillips.email>
2016-05-05 00:07:23 -0400

    aded audio, video, and reorganized content

--- Final Project/FMC/js/fmc.js
+++ Final Project/FMC/js/fmc.js
@@ -1,4 +1,15 @@
+/*Bootstrap JS for sidebar*/
 $('body').scrollspy({
     target: '.bs-docs-sidebar',
     offset: 40
 });
+
+/*Facebook plgin*/
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
