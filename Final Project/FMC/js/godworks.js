085a2914f6298a8703126c87ff54441d58e58e90
phillipsk <inquiries@kevinphillips.email>
2016-05-07 09:00:22 -0400

    working on js app god.works

---
+++ Final Project/FMC/js/godworks.js
@@ -0,0 +1,37 @@
+window.onload = function () {
+    var OldTestButton = document.getElementById('bible-list');
+
+};
+// <a class="btn btn-default" href="#" role="button">Genesis</a>
+
+var OldTestamentBooksLen = FullBooks - 22;
+var NewTestamentBooksLen = FullBooks - OldTestamentBooksLen;
+for (var i = 0; i < OldTestamentBooksLen; i++) {
+
+    //var el = ;
+
+    /*
+     var textLen = text.length;
+     document.getElementById("transcriptText").innerHTML = "";
+     for (var i = 0; i < textLen; i++) {
+     var word = text[i];
+
+     var wordSpan = document.createElement('span');
+     var spaceSpan = document.createElement('span');
+     var wordNode = document.createTextNode(word);
+     var spaceNode = document.createTextNode(' ');
+
+     wordSpan.addEventListener('mouseover', function () {
+     this.setAttribute('class', 'highlight')
+     });
+     wordSpan.addEventListener('mouseout', function (evt) {
+     this.setAttribute('class', 'default')
+     });
+
+     wordSpan.appendChild(wordNode);
+     spaceSpan.appendChild(spaceNode);
+     textDiv.appendChild(wordSpan);
+     textDiv.appendChild(spaceSpan);
+     }
+     */
+}
